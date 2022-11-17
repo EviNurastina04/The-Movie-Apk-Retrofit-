c39bca6982d52bddd73370acc0ea4933d89b2041
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-11-17 08:39:10 +0700

    Update and rename BuildConfig.java to BuildConfig.kt

--- BuildConfig.kt
+++ BuildConfig.kt
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.lazday.androidretrofit;
+package com.themovie.androidretrofit;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.lazday.androidretrofit";
+  public static final String APPLICATION_ID = "com.themovie.androidretrofit";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
