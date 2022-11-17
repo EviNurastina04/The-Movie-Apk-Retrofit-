bf7d354a901ba788af1832dcf2c6630a2f8ee5cd
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-11-17 08:42:57 +0700

    Update and rename ExampleInstrumentedTest.java to ExampleInstrumentedTest.kt

--- ExampleInstrumentedTest.kt
+++ ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.lazday.androidretrofit;
+package com.themovie.androidretrofit;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.lazday.androidretrofit", appContext.getPackageName());
+        assertEquals("com.themovie.androidretrofit", appContext.getPackageName());
     }
 }
